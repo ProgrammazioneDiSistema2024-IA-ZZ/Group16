23fc13aee43e9dc6eb2f0bab624f8b430c40b91a
FRANCESCA VILLANOVA s329950 <FRANCESCA VILLANOVA s329950@studenti.polito.it>
2024-09-27 16:26:28 +0200

    CPU Monitoring

---
+++ src/cpu_evaluation.rs
@@ -0,0 +1,28 @@
+use sysinfo::{System, Pid};
+use std::{fs::OpenOptions};
+use chrono::Local;
+use std::io::Write;
+
+pub fn create_file() -> std::fs::File {
+        OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("cpu_log.txt")
+        .unwrap()
+}
+
+pub fn process_cpu_consumption(pid: Pid, mut cpu_log_file: &mut std::fs::File) {
+    // Create a system object
+    let mut sys = System::new_all();
+
+
+    // Refresh the CPU consumption
+    sys.refresh_cpu_usage();
+
+    if let Some(process) = sys.process(pid) {
+        let current_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        let log_entry = format!("{} - CPU Usage: {}%", current_time, process.cpu_usage());
+        writeln!(cpu_log_file, "{}", log_entry).unwrap();
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod cpu_evaluation;
+use std::process;
+use sysinfo::Pid;
+use std::{thread, time};
+
 fn main() {
+
     println!("Hello, world!");
+
+    let mut cpu_log_file = cpu_evaluation::create_file();
+    let pid = Pid::from_u32(process::id());
+
+    loop {
+        cpu_evaluation::process_cpu_consumption(pid, &mut cpu_log_file);
+
+        //Sleep for 2 minutes
+        thread::sleep(time::Duration::from_secs(120));
+    }
+
 }
+
