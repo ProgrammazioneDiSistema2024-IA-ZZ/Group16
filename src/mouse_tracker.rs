3f4bd70213b44a266efed9dbb712a6b94ae4f67e
FRANCESCA VILLANOVA s329950 <FRANCESCA VILLANOVA s329950@studenti.polito.it>
2024-10-03 16:54:02 +0200

    audio capo

--- src/mouse_tracker.rs
+++ src/mouse_tracker.rs
@@ -1,7 +1,7 @@
 use rdev::{listen, Event, EventType};
 use std::sync::{Arc, Mutex};
-use std::thread;
-
+use std::{fs, thread};
+use std::process::Command;
 use crate::audio::play_sound;
 use crate::{backup};
 
@@ -11,9 +11,21 @@ struct Point{
     y: f64,
 }
 
+#[derive(Debug, PartialEq)]
+enum Action {
+    Background,
+    Confirm,
+    Cancel,
+    Modify,
+}
+
 /// Funzione per controllare se un punto è vicino a un altro punto, entro una certa tolleranza
-fn is_near(p1: &Point, p2: &Point, tolerance: f64) -> bool {
-    distance(p1, p2) <= tolerance
+fn is_near(p1: &Point, p2: &Point, tolerance: f64) -> Action {
+    if distance(p1, p2) <= tolerance {
+        Action::Confirm
+    } else {
+        Action::Cancel
+    }
 }
 
 /// Funzione per controllare se il vettore di punti contiene i punti agli angoli dello schermo
@@ -22,7 +34,7 @@ fn contains_corners(
     screen_width: f64,
     screen_height: f64,
     enable: bool
-) -> bool {
+) -> Action {
     let tolerance = 50.0; // Definisci una tolleranza per la vicinanza agli angoli
     if !enable {
         let top_left = Point { x: 0.00, y: 0.00 };
@@ -36,49 +48,58 @@ fn contains_corners(
         let mut found_bottom_right = false;
 
         for point in points {
-            if is_near(&point, &top_left, tolerance) {
+            if is_near(&point, &top_left, tolerance) == Action::Confirm{
                 found_top_left = true;
             }
-            if is_near(&point, &top_right, tolerance) {
+            if found_top_left && is_near(&point, &top_right, tolerance) == Action::Confirm{
                 found_top_right = true;
             }
-            if is_near(&point, &bottom_left, tolerance) {
-                found_bottom_left = true;
-            }
-            if is_near(&point, &bottom_right, tolerance) {
+            if found_top_right && is_near(&point, &bottom_right, tolerance) == Action::Confirm{
                 found_bottom_right = true;
+            }
+            if found_bottom_right && is_near(&point, &bottom_left, tolerance) == Action::Confirm{
+                found_bottom_left = true;
             }
         }
 
-        found_top_left && found_top_right && found_bottom_left && found_bottom_right
+        if found_top_left && found_top_right && found_bottom_left && found_bottom_right {
+            Action::Confirm
+        } else {
+            Action::Cancel
+        }
     } else {
         // Definisci gli angoli necessari
         let bottom_left = Point { x: 0.00, y: screen_height };
         let bottom_right = Point { x: screen_width, y: screen_height };
         let top_left = Point { x: 0.00, y: 0.00 };
+        let top_right = Point { x: screen_width, y: 0.00 };
 
         let mut passed_bottom_left = false;
 
         for point in points {
             // Se il mouse è passato dall'angolo in basso a sinistra
-            if is_near(&point, &bottom_left, tolerance) {
+            if is_near(&point, &bottom_left, tolerance) == Action::Confirm {
                 passed_bottom_left = true;
             }
 
             if passed_bottom_left {
                 // Se il mouse va dall'angolo in basso a sinistra a quello in basso a destra, ritorna true
-                if is_near(&point, &bottom_right, tolerance) {
-                    return true;
+                if is_near(&point, &bottom_right, tolerance) == Action::Confirm {
+                    return Action::Confirm;
                 }
                 // Se il mouse va dall'angolo in basso a sinistra a quello in alto a sinistra, ritorna false
-                if is_near(&point, &top_left, tolerance) {
-                    return false;
+                if is_near(&point, &top_left, tolerance) == Action::Confirm {
+                    return Action::Cancel;
+                }
+                // Se il mouse va dall'angolo in basso a sinistra a quello in alto a destra, ritorna false
+                if is_near(&point, &top_right, tolerance) == Action::Confirm {
+                    return Action::Modify;
                 }
             }
         }
 
         // Se non è stato trovato nessun percorso specifico, ritorna false di default
-        false
+        Action::Cancel
     }
 }
 
@@ -109,15 +130,29 @@ pub fn track_mouse(window_enable: Arc<Mutex<bool>>, screen_width: f64, screen_he
                 println!("Punto tracciato: ({:.2}, {:.2})", point.x, point.y);
 
                 // Verifica se ci sono abbastanza punti per riconoscere gli angoli dello schermo
-                if !enabled && contains_corners(&points, screen_width, screen_height, enabled) {
+                if !enabled && contains_corners(&points, screen_width, screen_height, enabled) == Action::Confirm {
                     play_sound(0);
                     points.clear();
                     let mut enabled_ref = tracking_enabled_clone.lock().unwrap();
                     *enabled_ref = true;  // Cambia qui lo stato di tracking_enabled
                 }
 
+                if !enabled && contains_corners(&points, screen_width, screen_height, enabled) == Action::Modify {
+                    fs::remove_file("config.toml").unwrap();
+                    // Start the config_program and capture its PID
+                    let config_program = Command::new("cargo")
+                        .arg("run")
+                        .arg("--bin")
+                        .arg("config_program")
+                        .output();  // `spawn` instead of `output` to get the PID
+
+                    points.clear();
+                    let mut enabled_ref = tracking_enabled_clone.lock().unwrap();
+                    *enabled_ref = true;  // Cambia qui lo stato di tracking_enabled
+                }
+
                 // Se il tracciamento è abilitato, verifica se viene disegnato un "+", e non solo gli angoli
-                if enabled && contains_corners(&points, screen_width, screen_height, enabled) {
+                if enabled && contains_corners(&points, screen_width, screen_height, enabled) == Action::Confirm {
 
                     let config = backup::read_config("config.toml");
 
@@ -144,6 +179,12 @@ pub fn track_mouse(window_enable: Arc<Mutex<bool>>, screen_width: f64, screen_he
                     let mut enabled_ref = tracking_enabled_clone.lock().unwrap();
                     *enabled_ref = false;  // Cambia qui lo stato di tracking_enabled
                     //std::process::exit(0);  // Esci dal programma quando viene trovato un "+"
+                }
+                if enabled && contains_corners(&points, screen_width, screen_height, enabled) == Action::Cancel {
+                    play_sound(2);
+                    points.clear();
+                    let mut enabled_ref = tracking_enabled_clone.lock().unwrap();
+                    *enabled_ref = false;  // Cambia qui lo stato di tracking_enabled
                 }
             }
         }).unwrap();
